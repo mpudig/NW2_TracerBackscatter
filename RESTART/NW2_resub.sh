61df09dd5441640f067ce9bc86ec26f083cadb79
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2024-08-28 16:33:10 -0400

    Default configuration for 1/4 degree spin up with one tracer time scale.

--- RESTART/NW2_resub.sh
+++ RESTART/NW2_resub.sh
@@ -4,15 +4,15 @@
 #SBATCH --ntasks-per-node=48
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16GB
-#SBATCH --time=48:00:00
-#SBATCH --job-name=EXP_NAME
+#SBATCH --time=72:00:00
+#SBATCH --job-name=p25_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=EXP_NAME
+EXP_NAME=p25_SpinUp
 
 ## Copy restart file from RESTART to INPUT within same experiment folder
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/
--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -4,15 +4,15 @@
 #SBATCH --ntasks-per-node=48
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16GB
-#SBATCH --time=48:00:00
-#SBATCH --job-name=EXP_NAME
+#SBATCH --time=72:00:00
+#SBATCH --job-name=p25_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=EXP_NAME
+EXP_NAME=p25_SpinUp
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
 rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
@@ -25,5 +25,5 @@ module purge
 source ~/NeverWorld2/build/intel/env
 srun ~/NeverWorld2/build/intel/ocean_only/repro/MOM6
 
-## Copy slurm files and remove work folder in home
+## Copy slurm files
 cp ~/NW2_TracerBackscatter/$EXP_NAME/RESTART/slurm* $SCRATCH/NW2_Tracer_Backscatter/$EXP_NAME/RESTART
