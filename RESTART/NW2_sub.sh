afaadd0e6e275fe8c89e28885bcab72268475e75
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2024-12-11 22:10:16 -0500

    Default analysis for p5 run.

--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
+#SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16GB
-#SBATCH --time=24:00:00
-#SBATCH --job-name=p5_EBTBS_KHTR0_9Tracer
+#SBATCH --mem=10GB
+#SBATCH --time=1:00:00
+#SBATCH --job-name=p5_EBTBS_KHTR0
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_EBTBS_KHTR0_9Tracer
+EXP_NAME=p5_EBTBS_KHTR0
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
 rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 mkdir -p $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cp -r ~/NW2_TracerBackscatter/$EXP_NAME/* .
+cp $SCRATCH/NW2_TracerBackscatter/p5_EBTBS_KHTR0_9Tracer_SpinUp/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT
 
 ## Run the model
 module purge
