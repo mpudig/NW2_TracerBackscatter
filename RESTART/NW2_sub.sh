449618266741adfcb16e78fce98f0cc437ca49d3
Matt Pudig <mp6191@log-1.hpc.nyu.edu>
2025-01-27 09:46:02 -0500

    Default configuration for dye tracer over surface.

--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=42
+#SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8GB
-#SBATCH --time=1:00:00
-#SBATCH --job-name=p5_noBS_KHTR0
+#SBATCH --time=00:2:00
+#SBATCH --job-name=p5_noBS_KHTR0_dye
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_noBS_KHTR0
+EXP_NAME=p5_noBS_KHTR0_dye
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
 rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 mkdir -p $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cp -r ~/NW2_TracerBackscatter/$EXP_NAME/* .
-cp $SCRATCH/NW2_TracerBackscatter/p5_noBS_KHTR0_9Tracer_SpinUp/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT
+mv $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT/MOM.res.dye.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT/MOM.res.nc
 
 ## Run the model
 module purge
