978f2c7ebe323b610cbdd967341f9db60c40be8f
Matt Pudig <mp6191@log-3.hpc.nyu.edu>
2025-02-12 16:42:05 -0500

    Default config for spinning up 1/4 deg unparam from rest with 8 tracers.

--- RESTART/NW2_resub.sh
+++ RESTART/NW2_resub.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=10GB
-#SBATCH --time=16:00:00
-#SBATCH --job-name=p5_noBS_KHTR0_SpinUp
+#SBATCH --mem=25GB
+#SBATCH --time=24:00:00
+#SBATCH --job-name=p25_noBS_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_noBS_KHTR0_SpinUp
+EXP_NAME=p25_noBS_SpinUp
 
 ## Copy restart file from RESTART to INPUT within same experiment folder
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/
--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=10GB
-#SBATCH --time=12:00:00
-#SBATCH --job-name=p5_noBS_KHTR0_SpinUp
+#SBATCH --mem=25GB
+#SBATCH --time=72:00:00
+#SBATCH --job-name=p25_noBS_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_noBS_KHTR0_SpinUp
+EXP_NAME=p25_noBS_SpinUp
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
 rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
