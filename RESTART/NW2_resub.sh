2aa604835f7c251e4b1b0b9314ea91c7c9090298
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2025-02-17 12:54:03 -0500

    Default testing config for 1/4 deg SQG backscatter.

--- RESTART/NW2_resub.sh
+++ RESTART/NW2_resub.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16GB
-#SBATCH --time=48:00:00
-#SBATCH --job-name=p5_EBTBS_KHTR0
+#SBATCH --time=12:00:00
+#SBATCH --job-name=p25_SQG_testing1
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_EBTBS_KHTR0
+EXP_NAME=p25_SQG_testing1
 
 ## Copy restart file from RESTART to INPUT within same experiment folder
-cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/
+cd $SCRATCH/NW2_TracerBackscatter/p25_SQG_testing/$EXP_NAME
 cp RESTART/MOM.res.nc INPUT
 
 ## Change input.nml file to accept restart
 sed -i "/^         input_filename = 'F'/s/input_filename = 'F'/input_filename = 'r'/g" input.nml
+
+## Change name of ocean stats                                                                                                                                                                               
+sed -i 's/ENERGYFILE = "ocean.stats"/ENERGYFILE = "ocean.stats_1"/g' MOM_override
 
 ## Run the model
 module purge
--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -3,22 +3,23 @@
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16GB
-#SBATCH --time=14:00:00
-#SBATCH --job-name=p25_EBT_testing1
+#SBATCH --mem=15GB
+#SBATCH --time=8:00:00
+#SBATCH --job-name=p25_SQG_testing1
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p25_EBT_testing1
+EXP_NAME=p25_SQG_testing1
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
-rm -rf $SCRATCH/NW2_TracerBackscatter/p25_EBT_testing/$EXP_NAME
-mkdir -p $SCRATCH/NW2_TracerBackscatter/p25_EBT_testing/$EXP_NAME
-cd $SCRATCH/NW2_TracerBackscatter/p25_EBT_testing/$EXP_NAME
-cp -r ~/NW2_TracerBackscatter/p25_EBT_testing/$EXP_NAME/* .
+rm -rf $SCRATCH/NW2_TracerBackscatter/p25_SQG_testing/$EXP_NAME
+mkdir -p $SCRATCH/NW2_TracerBackscatter/p25_SQG_testing/$EXP_NAME
+cd $SCRATCH/NW2_TracerBackscatter/p25_SQG_testing/$EXP_NAME
+cp -r ~/NW2_TracerBackscatter/p25_SQG_testing/$EXP_NAME/* .
+cp ~/NW2_TracerBackscatter/p25_SQG_testing/p25_SQGBS_interp_IC_thickness.nc INPUT/
 
 ## Run the model
 module purge
@@ -26,4 +27,4 @@ source ~/NeverWorld2/build/intel/env
 srun ~/NeverWorld2/build/intel/ocean_only/repro/MOM6
 
 ## Copy slurm files
-cp ~/NW2_TracerBackscatter/p25_EBT_testing/$EXP_NAME/RESTART/slurm* $SCRATCH/NW2_TracerBackscatter/p25_EBT_testing/$EXP_NAME/RESTART
+cp ~/NW2_TracerBackscatter/p25_SQG_testing/$EXP_NAME/RESTART/slurm* $SCRATCH/NW2_TracerBackscatter/p25_SQG_testing/$EXP_NAME/RESTART
