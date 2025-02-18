7be4aad35f90e61e70cbdc94e96532b9b120da56
Matt Pudig <mp6191@log-1.hpc.nyu.edu>
2025-02-18 16:41:14 -0500

    Default config with 8 tracers.

--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8GB
+#SBATCH --mem=10GB
 #SBATCH --time=1:00:00
 #SBATCH --job-name=p5_noBS_KHTR0
 #SBATCH --output=slurm_%j.out
@@ -19,7 +19,7 @@ rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 mkdir -p $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cp -r ~/NW2_TracerBackscatter/$EXP_NAME/* .
-cp $SCRATCH/NW2_TracerBackscatter/p5_noBS_KHTR0_9Tracer_SpinUp/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT
+cp $SCRATCH/NW2_TracerBackscatter/p5_noBS_KHTR0_SpinUp/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT/MOM.res.nc
 
 ## Run the model
 module purge
