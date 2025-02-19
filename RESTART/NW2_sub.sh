99d09991743dec39d3969dcac121ea480a081205
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2025-02-19 13:45:17 -0500

    Default analysis config for 1/2 SQG backscatter.

--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -5,21 +5,21 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10GB
 #SBATCH --time=2:00:00
-#SBATCH --job-name=p5_EBTBS_KHTR0
+#SBATCH --job-name=p5_SQGBS_KHTR0
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_EBTBS_KHTR0
+EXP_NAME=p5_SQGBS_KHTR0
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
 rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 mkdir -p $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cp -r ~/NW2_TracerBackscatter/$EXP_NAME/* .
-cp $SCRATCH/NW2_TracerBackscatter/p5_EBTBS_KHTR0_SpinUp/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT/MOM.res.nc
+cp $SCRATCH/NW2_TracerBackscatter/p5_SQGBS_KHTR0_SpinUp/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT/MOM.res.nc
 
 ## Run the model
 module purge
