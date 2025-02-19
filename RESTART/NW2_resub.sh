26855e791dc69933408cbe2fc10156352f1a0862
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2025-02-19 14:34:40 -0500

    Default config for spinning up 1/2 SQG backscatter.

--- RESTART/NW2_resub.sh
+++ RESTART/NW2_resub.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
+#SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16GB
-#SBATCH --time=48:00:00
-#SBATCH --job-name=p5_EBTBS_KHTR0
+#SBATCH --mem=10GB
+#SBATCH --time=3:00:00
+#SBATCH --job-name=p5_SQGBS_KHTR0_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_EBTBS_KHTR0
+EXP_NAME=p5_SQGBS_KHTR0_SpinUp
 
 ## Copy restart file from RESTART to INPUT within same experiment folder
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/
@@ -20,6 +20,9 @@ cp RESTART/MOM.res.nc INPUT
 
 ## Change input.nml file to accept restart
 sed -i "/^         input_filename = 'F'/s/input_filename = 'F'/input_filename = 'r'/g" input.nml
+
+## Change name of ocean stats
+sed -i 's/ENERGYFILE = "ocean.stats"/ENERGYFILE = "ocean.stats_1"/g' MOM_override
 
 ## Run the model
 module purge
--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -4,22 +4,21 @@
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16GB
-#SBATCH --time=6:00:00
-#SBATCH --job-name=SQG_testing2
+#SBATCH --time=48:00:00
+#SBATCH --job-name=p5_SQGBS_KHTR0_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_SQG_testing2
+EXP_NAME=p5_SQGBS_KHTR0_SpinUp
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
-rm -rf $SCRATCH/NW2_TracerBackscatter/p5_SQG_testing/$EXP_NAME
-mkdir -p $SCRATCH/NW2_TracerBackscatter/p5_SQG_testing/$EXP_NAME
-cd $SCRATCH/NW2_TracerBackscatter/p5_SQG_testing/$EXP_NAME
-cp -r ~/NW2_TracerBackscatter/p5_SQG_testing/$EXP_NAME/* .
-cp $SCRATCH/NW2_TracerBackscatter/p5_EBTBS_KHTR0/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/p5_SQG_testing/$EXP_NAME/INPUT
+rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
+mkdir -p $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
+cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
+cp -r ~/NW2_TracerBackscatter/$EXP_NAME/* .
 
 ## Run the model
 module purge
@@ -27,4 +26,4 @@ source ~/NeverWorld2/build/intel/env
 srun ~/NeverWorld2/build/intel/ocean_only/repro/MOM6
 
 ## Copy slurm files
-cp ~/NW2_TracerBackscatter/p5_SQG_testing/$EXP_NAME/RESTART/slurm* $SCRATCH/NW2_TracerBackscatter/p5_SQG_testing/$EXP_NAME/RESTART
+cp ~/NW2_TracerBackscatter/$EXP_NAME/RESTART/slurm* $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/RESTART
