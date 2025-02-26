8548cf16f522b0108470c8540a6f37f440ed887d
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2025-02-26 17:10:10 -0500

    Default config for 1/4 unparameterized with NW2 tracers.

--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=10GB
-#SBATCH --time=1:00:00
-#SBATCH --job-name=p5_noBS_KHTR0
+#SBATCH --mem=15GB
+#SBATCH --time=2:30:00
+#SBATCH --job-name=p25_noBS
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_noBS_KHTR0
+EXP_NAME=p25_noBS
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
 rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 mkdir -p $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
 cp -r ~/NW2_TracerBackscatter/$EXP_NAME/* .
-cp $SCRATCH/NW2_TracerBackscatter/p5_noBS_KHTR0_SpinUp/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT/MOM.res.nc
+cp $SCRATCH/NW2_TracerBackscatter/p25_noBS_SpinUp/RESTART/MOM.res.nc $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/INPUT/MOM.res.nc
 
 ## Run the model
 module purge
