2abbdfde0ce6aade72f345971edf67b765638c8b
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2025-02-26 17:24:10 -0500

    Default config for 1/4 deg spin up with SQG backscatter and NW2 tracers.

--- RESTART/NW2_resub.sh
+++ RESTART/NW2_resub.sh
@@ -3,16 +3,16 @@
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=25GB
-#SBATCH --time=24:00:00
-#SBATCH --job-name=p25_noBS_SpinUp
+#SBATCH --mem=16GB
+#SBATCH --time=48:00:00
+#SBATCH --job-name=p25_SQGBS_KHTR0_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p25_noBS_SpinUp
+EXP_NAME=p25_SQGBS_KHTR0_SpinUp
 
 ## Copy restart file from RESTART to INPUT within same experiment folder
 cd $SCRATCH/NW2_TracerBackscatter/$EXP_NAME/
@@ -20,6 +20,9 @@ cp RESTART/MOM.res.nc INPUT
 
 ## Change input.nml file to accept restart
 sed -i "/^         input_filename = 'F'/s/input_filename = 'F'/input_filename = 'r'/g" input.nml
+
+## Change name of ocean stats                                                                                                                                                                              
+sed -i 's/ENERGYFILE = "ocean.stats"/ENERGYFILE = "ocean.stats_1"/g' MOM_override
 
 ## Run the model
 module purge
--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -3,16 +3,16 @@
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=25GB
-#SBATCH --time=72:00:00
-#SBATCH --job-name=p25_noBS_SpinUp
+#SBATCH --mem=16GB
+#SBATCH --time=48:00:00
+#SBATCH --job-name=p25_SQGBS_KHTR0_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p25_noBS_SpinUp
+EXP_NAME=p25_SQGBS_KHTR0_SpinUp
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
 rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
