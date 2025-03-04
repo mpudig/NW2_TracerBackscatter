2e4c0217368ffe63a0309841867376d06fa96c68
Matt Pudig <mp6191@log-2.hpc.nyu.edu>
2025-03-04 07:36:38 -0500

    Default 500d config for 1/2 deg backscatter with 100m surface dye.

--- RESTART/NW2_sub.sh
+++ RESTART/NW2_sub.sh
@@ -3,16 +3,16 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8GB
-#SBATCH --time=3:00:00
-#SBATCH --job-name=p5_noBS_KHTR0_dye
+#SBATCH --mem=12GB
+#SBATCH --time=1:00:00
+#SBATCH --job-name=p5_SQGBS_KHTR0_dye
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mp6191@nyu.edu
 
 ## Set experiment name here
-EXP_NAME=p5_noBS_KHTR0_dye
+EXP_NAME=p5_SQGBS_KHTR0_dye
 
 ## Create an output directory /scratch/mp6191/NW2_TracerBackscatter/EXP_NAME and copy experiment files into it
 rm -rf $SCRATCH/NW2_TracerBackscatter/$EXP_NAME
