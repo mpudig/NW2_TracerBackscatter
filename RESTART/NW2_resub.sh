c2379de4e14e50bcd6be48dca36ed4da658574ab
Matt Pudig <mp6191@log-1.hpc.nyu.edu>
2025-05-12 21:06:09 -0400

    Update.

--- RESTART/NW2_resub.sh
+++ RESTART/NW2_resub.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=42
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10GB
-#SBATCH --time=16:00:00
+#SBATCH --time=4:00:00
 #SBATCH --job-name=p5_noBS_KHTR0_SpinUp
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
@@ -20,6 +20,9 @@ cp RESTART/MOM.res.nc INPUT
 
 ## Change input.nml file to accept restart
 sed -i "/^         input_filename = 'F'/s/input_filename = 'F'/input_filename = 'r'/g" input.nml
+
+## Change name of ocean stats
+sed -i 's/ENERGYFILE = "ocean.stats_3"/ENERGYFILE = "ocean.stats_4"/g' MOM_override
 
 ## Run the model
 module purge
